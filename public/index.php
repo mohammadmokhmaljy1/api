9a3ebc1f171c299d21fad05fa1593c5eef729da0
Mohammad Mokhmaljy <noorhhsas@gmail.com>
2025-02-03 13:38:19 +0300

    fix routers

--- public/index.php
+++ public/index.php
@@ -43,11 +43,11 @@ $router->add('/ordering/show/(\d+)', 'OrderingController@show');
 $router->add('/ordering/update/(\d+)', 'OrderingController@update');
 $router->add('/ordering/delete/(\d+)', 'OrderingController@delete');
 
-$router->add('/product', 'ProductController@index');
-$router->add('/product/create', 'ProductController@create');
-$router->add('/product/show/(\d+)', 'ProductController@show');
-$router->add('/product/update/(\d+)', 'ProductController@update');
-$router->add('/product/delete/(\d+)', 'ProductController@delete');
+$router->add('/products', 'ProductController@index');
+$router->add('/products/create', 'ProductController@create');
+$router->add('/products/show/(\d+)', 'ProductController@show');
+$router->add('/products/update/(\d+)', 'ProductController@update');
+$router->add('/products/delete/(\d+)', 'ProductController@delete');
 
 $router->add('/product-image', 'ProductImageController@index');
 $router->add('/product-image/create', 'ProductImageController@create');
